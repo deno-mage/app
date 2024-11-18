3364961039cc655683aa2a60c73628e5401bc4cc
Lee Cheneler <lacheneler@gmail.com>
2024-11-18 15:15:11 +0000

    feat: added cors and redirect tests (#4)

---
+++ tests/middleware/cors/allow-headers.test.ts
@@ -0,0 +1,51 @@
+import { afterAll, beforeAll, describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+import { StatusCode, useCors } from "../../../exports.ts";
+import { MageTestServer } from "../../../test-utils/server.ts";
+
+let server: MageTestServer;
+
+beforeAll(() => {
+  server = new MageTestServer();
+
+  server.app.options(
+    "/",
+    useCors({
+      headers: ["X-Custom-Header-One", "X-Custom-Header-Two"],
+    }),
+  );
+  server.app.get(
+    "/",
+    useCors({
+      headers: ["X-Custom-Header-One", "X-Custom-Header-Two"],
+    }),
+  );
+
+  server.start();
+});
+
+afterAll(async () => {
+  await server.stop();
+});
+
+describe("middleware - cors - allow headers", () => {
+  it("should not send back header when GET", async () => {
+    const response = await fetch(server.url("/"), {
+      method: "GET",
+    });
+
+    expect(response.status).toEqual(StatusCode.NoContent);
+    expect(response.headers.get("Access-Control-Allow-Headers")).toBeNull();
+  });
+
+  it("should send back header when OPTIONS", async () => {
+    const response = await fetch(server.url("/"), {
+      method: "OPTIONS",
+    });
+
+    expect(response.status).toEqual(StatusCode.NoContent);
+    expect(response.headers.get("Access-Control-Allow-Headers")).toBe(
+      "X-Custom-Header-One, X-Custom-Header-Two",
+    );
+  });
+});
---
+++ tests/middleware/cors/allow-methods.test.ts
@@ -0,0 +1,51 @@
+import { afterAll, beforeAll, describe, it } from "@std/testing/bdd";
+import { expect } from "@std/expect";
+import { HttpMethod, StatusCode, useCors } from "../../../exports.ts";
+import { MageTestServer } from "../../../test-utils/server.ts";
+
+let server: MageTestServer;
+
+beforeAll(() => {
+  server = new MageTestServer();
+
+  server.app.options(
+    "/",
+    useCors({
+      methods: [HttpMethod.Delete, HttpMethod.Get, HttpMethod.Options],
+    }),
+  );
+  server.app.get(
+    "/",
+    useCors({
+      methods: [HttpMethod.Delete, HttpMethod.Get, HttpMethod.Options],
+    }),
+  );
+
+  server.start();
+});
+
+afterAll(async () => {
+  await server.stop();
+});
+
+describe("middleware - cors - allow methods", () => {
+  it("should not send back header when GET", async () => {
+    const response = await fetch(server.url("/"), {
+      method: "GET",
+    });
+
+    expect(response.status).toEqual(StatusCode.NoContent);
+    expect(response.headers.get("Access-Control-Allow-Methods")).toBeNull();
+  });
+
+  it("should send back header when OPTIONS", async () => {
+    const response = await fetch(server.url("/"), {
+      method: "OPTIONS",
+    });
+
+    expect(response.status).toEqual(StatusCode.NoContent);
+    expect(response.headers.get("Access-Control-Allow-Methods")).toBe(
+      "DELETE, GET, OPTIONS",
+    );
+  });
+});
